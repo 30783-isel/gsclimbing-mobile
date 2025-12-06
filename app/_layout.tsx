fa36fc11aa1be3da74624c2f5efaf38ea6d0b215
Paulo Estrela <janganga@gmail.com>
2025-12-06 13:22:37 +0000

    wip

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,21 @@
 // app/_layout.tsx
 import { Stack } from 'expo-router';
+import { PaperProvider } from 'react-native-paper';
+import { theme } from '@/constants/theme';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="reports/defect-inspection/create"
-        options={{
-          presentation: 'modal',
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <PaperProvider theme={theme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="reports/defect-inspection/create"
+          options={{
+            presentation: 'modal',
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </PaperProvider>
   );
 }
